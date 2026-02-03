c38b037136d68990ea18b739d9ea27d0d40dc7c8
saidmera <SAIDMERA6@gmail.com>
2026-02-03 22:43:02 +0100

    Fix: Localized all CDN assets for 100% offline support

---
+++ public/js/react-dom.js
@@ -0,0 +1,4 @@
+/* esm.sh - react-dom@19.0.0 */
+import "/react@19.0.0/es2022/react.mjs";
+export * from "/react-dom@19.0.0/es2022/react-dom.mjs";
+export { default } from "/react-dom@19.0.0/es2022/react-dom.mjs";
